492529a0fbe3a25d4d1ad3b826b31cfc7aaadee9
Gonzalo <t3t34ge4awrfawqnfhnedr@gmail.com>
2023-08-07 08:12:53 -0300

    Realice el grid

---
+++ src/components/Grid.js
@@ -0,0 +1,27 @@
+import styles from "../styles/GridStyle.module.css";
+
+function Grid() {
+    return (
+        <div className={styles["conteinerClass"]}>
+            <div className={styles["navbarConteiner"]}>
+          
+            </div>
+            <div className={styles["headerConteiner"]}>
+
+            </div>
+            <div className={styles["contentConteiner"]}>
+              <div className={styles["descriptionConteiner"]}>
+                
+              </div>
+              <div className={styles["sidebarConteiner"]}>
+
+              </div>
+            </div>
+            <div className={styles["footerConteiner"]}>
+
+            </div>
+        </div>
+    );
+};
+
+export default Grid;
